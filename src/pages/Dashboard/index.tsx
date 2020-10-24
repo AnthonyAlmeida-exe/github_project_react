7e285d24de04e295ecca90579d8e6de7aa883d84
Anthony Luiz Almeida da Silva <anthony.silva@pampa.compasso>
2020-10-24 16:17:43 -0300

    screen search repo finished

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,81 @@
-import React from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, List, Error } from './styles';
 
-import { Title, Form, List } from './styles';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [repos, setRepos] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubeExplore:repos');
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+    try {
+      const response = await api.get(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepos([...repos, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca deste repositório!');
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('@GithubeExplore:repos', JSON.stringify(repos));
+  }, [repos]);
+
   return (
     <>
       <Title>Explore Repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          placeholder="Digite o nome do repositório"
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
-      <List />
+      {inputError && <Error>{inputError}</Error>}
+      <List>
+        {repos.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </List>
     </>
   );
 };
